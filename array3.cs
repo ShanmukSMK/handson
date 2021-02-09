c465c2ebd4ab5620c355383243dae6be91870960
ShanmukSMK <77377750+ShanmukSMK@users.noreply.github.com>
2021-02-09 07:52:06 +0530

    C#Assignment2

---
+++ array3.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class array3
+    {
+        static void Main()
+        {
+            int[] arr = new int[10];
+            Console.WriteLine("Enter the array elements");
+            for(int i=0;i<10;i++)
+            {
+                arr[i] = Convert.ToInt16(Console.ReadLine());
+            }
+            Console.WriteLine("Array Elements are");
+            for (int i = 0; i < 10; i++)
+                Console.Write(arr[i]+"\t");
+            int max = arr[0];
+            int pos=0;
+            for(int i=1;i<10;i++)
+            {
+                if(max<arr[i])
+                {
+                    max = arr[i];
+                    pos = i;
+                }
+            }
+            Console.WriteLine("\nMaximum element in the array is\n" + max);
+            Console.WriteLine("\nits position is\n" + pos);
+        }
+    }
+}
